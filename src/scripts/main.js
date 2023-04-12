5059762918811c820e1c36d0d189c8a5d1c4ec5d
popeye88 <aleksei.taran@gmail.com>
2023-04-12 12:34:02 +0300

    solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const employees = [...document.querySelectorAll('li')];
+const sortedEmployees = employees
+  .sort(
+    (a, b) => (
+      +b.dataset.salary.replace(/[$,]/g, '')
+      - +a.dataset.salary.replace(/[$,]/g, '')
+    )
+  );
+
+ul.append(...sortedEmployees);
+
+function getEmployees(list) {
+  const employeesList = list.forEach(el => {
+    return {
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    };
+  });
+
+  return employeesList;
+}
+
+getEmployees(employees);
